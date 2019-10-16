138230330d9e397b62701e08bf58a3dd6def16ae
Popoooo <sizuchico@gmail.com>
2019-10-16 16:36:37 +0900

    guu

--- python/divide.py
+++ python/divide.py
@@ -5,15 +5,15 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 
-current_dir = '../data/meigetsu'
-g_colab_full_path='/content/darknet/data/meigetsu'
+current_dir = '../data/guu'
+g_colab_full_path='/content/darknet/data/guu'
 
 # Percentage of images to be used for the test set
 percentage_test = 20;
 
 # Create and/or truncate train.txt and test.txt
-file_train = open('meigetsu-train.txt', 'w')  
-file_test = open('meigetsu-test.txt', 'w')# Populate train.txt and test.txt
+file_train = open('guu-train.txt', 'w')  
+file_test = open('guu-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
