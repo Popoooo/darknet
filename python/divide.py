0cf7a25e1f788920741e1365fd80fb09db8399e0
popoooo <sizuchico@gmail.com>
2020-02-29 16:52:32 +0900

    gravel

--- python/divide.py
+++ python/divide.py
@@ -5,15 +5,15 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 
-current_dir = '../data/kizu'
-g_colab_full_path='/content/darknet/data/kizu'
+current_dir = '../data/gravel'
+g_colab_full_path='/content/darknet/data/gravel'
 
 # Percentage of images to be used for the test set
 percentage_test = 20;
 
 # Create and/or truncate train.txt and test.txt
-file_train = open('kizu-train.txt', 'w')  
-file_test = open('kizu-test.txt', 'w')# Populate train.txt and test.txt
+file_train = open('gravel-train.txt', 'w')  
+file_test = open('gravel-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
